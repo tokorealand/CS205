862615e595638acb47a95b0939d5cdbfbd25397f
lopezl <lopezl@lafayette.edu>
2017-04-11 15:44:57 -0400

    "comment object"

--- repo_project/grader/comment.cpp
+++ repo_project/grader/comment.cpp
@@ -1,6 +1,28 @@
 #include "comment.h"
 
-Comment::Comment()
-{
+using namespace std;
 
+Comment::Comment(string commentText, string rubricSection, double pointsDeducted, string highlightColor){
+
+    this->commentText = commentText;
+    this->rubricSection = rubricSection;
+    this->pointsDeducted = pointsDeducted;
+    this->highlightColor = highlightColor;
+
+}
+
+string get_get_comment_text(){
+    return commentText;
+}
+
+string get_rubric_section(){
+    return rubricSection;
+}
+
+double get_points_deducted(){
+    return pointsDeducted;
+}
+
+string get_highlight_color(){
+    return highlightColor;
 }
